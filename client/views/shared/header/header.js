55182fd3335cde57019733178dd08ce8fd08b079
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-21 10:20:41 +0400

    Add sign in/sign up header

---
+++ client/views/shared/header/header.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* Header: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Header.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Header.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Header: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Header.created = function () {
+};
+
+Template.Header.rendered = function () {
+};
+
+Template.Header.destroyed = function () {
+};
