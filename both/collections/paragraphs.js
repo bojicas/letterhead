ba69c8d7a503378eddc309620afdb98b21d7dccc
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-23 23:00:53 +0400

    Add paragraphs collection

---
+++ both/collections/paragraphs.js
@@ -0,0 +1,7 @@
+Paragraphs = new Mongo.Collection('paragraphs');
+/*
+ * Add query methods like this:
+ *  Paragraphs.findPublic = function () {
+ *    return Paragraphs.find({is_public: true});
+ *  }
+ */
---
+++ server/collections/paragraphs.js
@@ -0,0 +1,33 @@
+/*
+ * Add query methods like this:
+ *  Paragraphs.findPublic = function () {
+ *    return Paragraphs.find({is_public: true});
+ *  }
+ */
+Paragraphs.allow({
+  insert: function (userId, doc) {
+    return true;
+  },
+
+  update: function (userId, doc, fieldNames, modifier) {
+    return true;
+  },
+
+  remove: function (userId, doc) {
+    return true;
+  }
+});
+
+Paragraphs.deny({
+  insert: function (userId, doc) {
+    return false;
+  },
+
+  update: function (userId, doc, fieldNames, modifier) {
+    return false;
+  },
+
+  remove: function (userId, doc) {
+    return false;
+  }
+});
