1513a5bda0357d9fd169b3574efb052cbc8ae792
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-23 12:40:23 +0400

    Add pages_show templates

---
+++ client/views/pages/pages_show/pages_show.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* PagesShow: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.PagesShow.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.PagesShow.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* PagesShow: Lifecycle Hooks */
+/*****************************************************************************/
+Template.PagesShow.created = function () {
+};
+
+Template.PagesShow.rendered = function () {
+};
+
+Template.PagesShow.destroyed = function () {
+};
