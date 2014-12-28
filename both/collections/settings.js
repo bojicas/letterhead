9cfac9d5c7d0d74f6dff5005c03d2423964ee975
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-28 18:05:44 +0000

    Add settings collection

---
+++ both/collections/settings.js
@@ -0,0 +1,7 @@
+Settings = new Mongo.Collection('settings');
+/*
+ * Add query methods like this:
+ *  Settings.findPublic = function () {
+ *    return Settings.find({is_public: true});
+ *  }
+ */
