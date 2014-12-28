181d726aa4debda534658ade4fdae3c94bdf37f6
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-28 15:48:26 +0000

    Assign/Change index page

--- client/views/pages/partials/pages_actions/pages_actions.js
+++ client/views/pages/partials/pages_actions/pages_actions.js
@@ -8,6 +8,17 @@ Template.PagesActions.events({
    *
    *  }
    */
+  'click .lh-make-index': function (e) {
+    e.preventDefault();
+
+    Pages.update({ _id: Pages.findOne({ type: 'INDEX' })._id }, {
+      $unset: { type: '' }
+    });
+    Pages.update({ _id: this._id }, {
+      $set: { type: 'INDEX' }
+    });
+    App.setAlert('Index page changed.', 'success');
+  }
 });
 
 Template.PagesActions.helpers({
@@ -17,6 +28,9 @@ Template.PagesActions.helpers({
    *    return Items.find();
    *  }
    */
+  indexPage: function () {
+    return this.type === 'INDEX';
+  }
 });
 
 /*****************************************************************************/
