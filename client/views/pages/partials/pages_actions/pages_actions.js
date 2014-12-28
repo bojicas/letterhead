a94cf7db10affb741184c1125ffc76723915ab34
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-28 15:28:10 +0000

    Mockup pages actions area

---
+++ client/views/pages/partials/pages_actions/pages_actions.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* PagesActions: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.PagesActions.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.PagesActions.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* PagesActions: Lifecycle Hooks */
+/*****************************************************************************/
+Template.PagesActions.created = function () {
+};
+
+Template.PagesActions.rendered = function () {
+};
+
+Template.PagesActions.destroyed = function () {
+};
