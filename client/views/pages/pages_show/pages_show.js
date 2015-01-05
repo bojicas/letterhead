1b9fcb9351fa3f6dbbee5147fd7e077229a46ce7
Silviu D. Bojica <silviu@cubicleapps.com>
2015-01-05 14:17:10 +0400

    Add disqus comments

--- client/views/pages/pages_show/pages_show.js
+++ client/views/pages/pages_show/pages_show.js
@@ -67,6 +67,23 @@ Template.PagesShow.rendered = function () {
   if (Session.get('smartEditor') === undefined) {
     Session.set('smartEditor', true);
   }
+
+  // Add disqus comments
+  var settings = Settings.findOne();
+  var page = Pages.findOne();
+  console.dir(settings);
+  console.dir(page);
+  console.log(settings.disqusShortname );
+  console.log(page.disqus);
+
+  if (settings.disqusShortname && page.disqus) {
+    console.log('I am here');
+    (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + settings.disqusShortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+  }
 };
 
 Template.PagesShow.destroyed = function () {
