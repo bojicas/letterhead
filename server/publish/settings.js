03d6bfdd8951285d667d4d1339c12243142ab82b
Silviu D. Bojica <silviu@cubicleapps.com>
2015-01-14 02:08:59 +0400

    Correct a typo

--- server/publish/settings.js
+++ server/publish/settings.js
@@ -5,5 +5,5 @@
 Meteor.publish(null, function () {
   // you can remove this if you return a cursor
   // this.ready();
-  return [Pages.find({ type: 'index' }), Settings.find()];
+  return [Pages.find({ type: 'INDEX' }), Settings.find()];
 });
