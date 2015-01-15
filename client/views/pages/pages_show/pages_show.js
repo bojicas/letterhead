c601856f7eb101b13a496955f428cd898c40f7e2
Silviu D. Bojica <silviu@cubicleapps.com>
2015-01-15 07:46:10 +0400

    Play around with Google Analytics

--- client/views/pages/pages_show/pages_show.js
+++ client/views/pages/pages_show/pages_show.js
@@ -81,6 +81,18 @@ Template.PagesShow.rendered = function () {
   if (Session.get('smartEditor') === undefined) {
     Session.set('smartEditor', true);
   }
+
+  var settings = Settings.findOne();
+  if (settings.googleTrackingId) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', settings.googleTrackingId, 'auto');
+    ga('send', 'pageview');
+  }
+
 };
 
 Template.PagesShow.destroyed = function () {
