a6ab121e4dd30d92261c5d5e8d73c95274b986d2
Silviu D. Bojica <silviu@cubicleapps.com>
2015-01-16 01:13:54 +0400

    Properly return 404 if page not found by _id or slug

--- server/publish/pages/pages_show.js
+++ server/publish/pages/pages_show.js
@@ -5,8 +5,12 @@
 Meteor.publish('pages_show', function (pageId) {
   check(pageId, String);
   var page = Pages.findOne({ $or: [ { _id: pageId }, { slug: pageId } ]});
-  return [
-    Pages.find({ $or: [ { _id: pageId }, { slug: pageId } ]}),
-    Paragraphs.find({ pageId: page._id }, { sort: { rank: 1 } })
-  ];
+  if (page) {
+    return [
+      Pages.find({ $or: [ { _id: pageId }, { slug: pageId } ]}),
+      Paragraphs.find({ pageId: page._id }, { sort: { rank: 1 } })
+    ];
+  } else {
+    return [];
+  }
 });
